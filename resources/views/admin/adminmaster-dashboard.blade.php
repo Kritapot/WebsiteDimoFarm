ae45ecd7ccdf4f19c6dbbb5f41029aadcd41f72e
Bank9999 <kritapot9999@gmail.com>
2019-06-18 17:29:04 +0700

    เพิ่ม stylesheet display id div

--- resources/views/admin/adminmaster-dashboard.blade.php
+++ resources/views/admin/adminmaster-dashboard.blade.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html>
-
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -10,16 +9,15 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/hidden-div.css') }}" rel="stylesheet">
 </head>
 
 <body class="hold-transition sidebar-mini">
     <div class="wrapper" id="adminmaster">
     @include('admin.layouts.menuside-bar')
-
         <div class="content-wrapper">
             <admin-main></admin-main>
         </div>
-
     </div>
     @include('admin.layouts.footer')
 
