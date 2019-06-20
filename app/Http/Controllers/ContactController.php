7808a22d50ec212caeb4ea0c25f75da324b72b74
Bank9999 <kritapot9999@gmail.com>
2019-06-20 19:46:37 +0700

    สร้าง route และ function save_contact ฝั่ง backend

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -21,7 +21,19 @@ class ContactController extends Controller
      */
     public function save_contact(Request $request)
     {
-        dd($request);
+        $this->validate($request, [
+
+            'contact_name'    =>'required|min:2|max:50',
+            'contact_email'   =>'required|min:2|max:50',
+
+        ]);
+
+        $saveContact                        =       new Contact();
+        $saveContact->contact_name          =       $request->contact_name;
+        $saveContact->contact_email         =       $request->contact_email;
+        $saveContact->contact_subject       =       $request->contact_subject;
+        $saveContact->contact_message       =       $request->contact_message;
+        $saveContact->save();
     }
 
 }
